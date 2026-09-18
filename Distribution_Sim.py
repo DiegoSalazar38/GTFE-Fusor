fc0539a7b035a128ba4af6007ddde43e9d5d3990
Salazar, Diego Alejandro <salazardiegoa22@gmail.com>
2026-09-18 14:22:05 -0400

    Created file for plotting distribution

---
+++ Distribution_Sim.py
@@ -0,0 +1,6 @@
+# The plasma we are working with is a good approximation of a perfect gas (low density, low pressure high temperature)
+# Goal is to plot the velocity (or kinetic energy) distribution function for the ions in the plasma
+# We want to compare the Maxwell-Boltzmann distribution to our result
+
+import plasmapy
+
